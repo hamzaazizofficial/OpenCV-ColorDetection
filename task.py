a8e87ea34516acaef0adab159586309221766401
hamzaazizofficial <37170062+hamzaazizofficial@users.noreply.github.com>
2020-01-14 20:52:16 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,22 @@
+# Importing Libraries
+import cv2 as cv
+import numpy as np
+
+# Importing Original Image
+img = cv.imread('1.jpg')
+# Showing Original Image
+cv.imshow('Original Image',img)  
+
+
+lower_range = np.array([0,0,0]) 
+upper_range = np.array([100,70,255])  
+mask = cv.inRange(img,lower_range,upper_range) 
+result = cv.bitwise_and(img,img,mask = mask)  
+cv.imshow('Color Detected Image',result) 
+
+
+cv.waitKey(0)
+
+#closing the windows
+cv.destroyWindow('Color Detected Image')
+cv.destroyWindow('Original Image')
